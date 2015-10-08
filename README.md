6d449532a5b4c13b0bf20a739279990e06476f9c
Seth Champagne <champagneSeth@gmail.com>
2015-10-08 16:18:18 -0500

    Changes to module to make it more modular

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ The Voice Command Node Module is a project by Team Sonus at Southeastern Louisia
 This is a module that uses CMU PocketSphinx to recognize audio and respond to voice commands in an event based system.
 
 Justin LeJeune
+
 Nick Moran
+
 Tyler Becker
-Seth Champange
+
+Seth Champagne
---
+++ sonus.js
@@ -0,0 +1,53 @@
+var PS          = require('lib/pocketsphinx.js')
+,   record      = require('lib/record')
+,   recognizer  = require('lib/recognizer.js')
+,   fs          = require('fs')
+;
+
+module.exports  = sonus;
+
+function sonus() {
+    var count = 3;
+    function countDown() {
+        console.log(count);
+        count -= 1;
+        if (count > 0) {
+            setTimeout(countDown, 1000);
+        } else {
+            console.log('Go!');
+            record('wav/woo', 3);
+            setTimeout(function () {
+                recognizeWav('wav/woo');
+            }, 5000)
+        }
+    }
+
+    recognizer.init();
+
+    console.log('\n\nRecording in...');
+    countDown();
+}
+
+function recognizeWav(fileName) {
+    var buffer  = new PS.AudioBuffer()
+    ,   audio   = fs.createReadStream(fileName + '.wav')
+    ,   output
+    ,   hyp
+    ;
+
+    audio.on('data', function (chunk) {
+        for (var i = 0; i < chunk.length; i += 1) {
+            buffer.push_back(chunk.readUInt8(i));
+        }
+    }).on('end', function () {
+        output = recognizer.start();
+        output = recognizer.process(buffer);
+        output = recognizer.stop();
+        hyp = recognizer.getHyp();
+        console.log(hyp);
+        console.log('done');
+        buffer.delete();
+        recognizer.delete();
+    });
+}
+
